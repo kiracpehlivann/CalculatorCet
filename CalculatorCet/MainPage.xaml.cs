1ea0db7aee69a9ef7aabcfe723571d46d9fd1090
Hüseyin Şimşek <Huseyin.Simsek@obase.com>
2024-11-26 14:45:10 +0300

    Add project files.

---
+++ CalculatorCet/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿namespace CalculatorCet
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        double FirstNumber;
+        bool isFirstNumberAfterOperator = true;
+        Operator PreviousOperator = Operator.None;
+
+
+        private void SubtractButton_Clicked(object sender, EventArgs e)
+        {
+            DoCalculation();
+            PreviousOperator = Operator.Subtract;
+
+        }
+
+        private void MultiplyButton_Clicked(object sender, EventArgs e)
+        {
+            DoCalculation();
+            PreviousOperator = Operator.Multiply;
+        }
+
+        private void DivideButton_Clicked(object sender, EventArgs e)
+        {
+            DoCalculation();
+            PreviousOperator = Operator.Divide;
+
+        }
+
+        private void AddButton_Clicked(object sender, EventArgs e)
+        {
+
+            DoCalculation();
+            PreviousOperator = Operator.Add;
+
+        }
+
+        void DoCalculation()
+        {
+
+            switch (PreviousOperator)
+            {
+                case Operator.None:
+                    FirstNumber = Double.Parse(Display.Text);
+                    break;
+                case Operator.Add:
+                    FirstNumber = FirstNumber + Double.Parse(Display.Text);
+                    break;
+                case Operator.Subtract:
+                    FirstNumber = FirstNumber - Double.Parse(Display.Text);
+
+                    break;
+                case Operator.Multiply:
+                    FirstNumber = FirstNumber * Double.Parse(Display.Text);
+
+                    break;
+                case Operator.Divide:
+                    FirstNumber = FirstNumber / Double.Parse(Display.Text);
+
+                    break;
+
+            }
+            isFirstNumberAfterOperator = true;
+            Display.Text = FirstNumber.ToString();
+        }
+
+        private void Digit_Clicked(object sender, EventArgs e)
+        {
+            Button digitButton = sender as Button;
+            if(isFirstNumberAfterOperator)
+            {
+                Display.Text = digitButton.Text;
+                isFirstNumberAfterOperator = false;
+
+            }
+            else
+            {
+                Display.Text += digitButton.Text;
+            }
+           
+        }
+    }
+
+}
